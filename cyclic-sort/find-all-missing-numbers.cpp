a6de50494a436de035b4e330602862d4a5dc5c22
Faith Anyanwu <111374405+faithscript@users.noreply.github.com>
2025-05-01 12:42:05 -0400

    Add find all missing numbers problem

---
+++ cyclic-sort/find-all-missing-numbers.cpp
@@ -0,0 +1,16 @@
+/*
+We are given an unsorted array containing numbers taken from the range 1 to 'n'. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+
+Example 1:
+
+Input: [2, 3, 1, 8, 2, 3, 5, 1]
+Output: 4, 6, 7
+Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+
+*/
+
+// Initialize the vector array with the example input
+vector<int> nums = {2, 3, 1, 8, 2, 3, 5, 1};  // Missing numbers: 4, 6, 7
+
+// the beautiful thing about cyclic sort is that we can use indices to our advantage, so the missing numbers will just be the indices that have the wrong numbers in them
+
