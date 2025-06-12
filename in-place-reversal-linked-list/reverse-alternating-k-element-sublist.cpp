9c3f2b78e8568cbed83f38f324063ab1bc530a7a
Faith Anyanwu <111374405+faithscript@users.noreply.github.com>
2025-06-12 17:57:10 -0400

    Update

---
+++ in-place-reversal-linked-list/reverse-alternating-k-element-sublist.cpp
@@ -0,0 +1,12 @@
+// Problem Statement
+// Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
+
+// If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+// Image
+// Constraints:
+
+// The number of nodes in the list is n.
+// 1 <= k <= n <= 5000
+// 0 <= Node.val <= 1000
+
